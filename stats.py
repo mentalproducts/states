ae4a7d3e583736a15c8b1e2ec3ed21cc7ab75c6f
mentalproducts <tadas.nu@gmail.com>
2014-03-20 15:22:10 +0200

    Add function count_allfiles which writes the statistics of elements of all files in the given directory to the given file

--- stats.py
+++ stats.py
@@ -9,9 +9,27 @@ from os.path import join
 
 data = {}
 
+write_file_o = open(write_file, 'w')
+
+
 for dir_entry in listdir(path):
     # Opens and reads directory entry if it is a file
     dir_entry_path = join(path, dir_entry)
     if isfile(dir_entry_path):
         with open(dir_entry_path, 'r') as my_file:
             data[dir_entry] = my_file.read()
+            
+            
+def count_allfiles(i):
+    count = {}
+    
+    for x in i:
+        if x in count:
+            count[x] += 1
+        else:
+            count[x] = 1
+
+    for key in count:
+        if count[key] > 0:
+            write_file_o.write(key + ':')
+            write_file_o.write(str(count[key]) + '\n')
