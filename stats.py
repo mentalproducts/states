a1f652920fccaf4df461f4c1e595965a105d99b1
mentalproducts <tadas.nu@gmail.com>
2014-03-20 15:17:34 +0200

    Add function dir_entry which opens and reads entry of a given directory if it is a file

--- stats.py
+++ stats.py
@@ -7,4 +7,11 @@ from os import listdir
 from os.path import isfile
 from os.path import join
 
+data = {}
 
+for dir_entry in listdir(path):
+    # Opens and reads directory entry if it is a file
+    dir_entry_path = join(path, dir_entry)
+    if isfile(dir_entry_path):
+        with open(dir_entry_path, 'r') as my_file:
+            data[dir_entry] = my_file.read()
